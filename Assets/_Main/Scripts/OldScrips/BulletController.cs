64f5d271400fbdd2e06cd0c1103b239a93a5f6c6
Jessica <jessica.galvan@hotmail.com>
2021-09-29 18:11:33 -0300

    Revert "Subida 1"
    This reverts commit 9766bc10674a61e2a2a7251f5d4c174ae4fff82d.

---
+++ Assets/_Main/Scripts/OldScrips/BulletController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField] private float speed = 0f;
+    [SerializeField] private int damage = 1;
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime,0,0); //la bala va a moverse hacia "adelante" del sentido en donde salio. 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        LifeController life = collision.GetComponent<LifeController>(); //Si el item tiene un life controller.... (ya la matrix de fisica va decidir si detecta la collision)
+        if (life != null) //si no tiene un life controller el item con el que collisiono, va a ser null. 
+        {
+            life.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/_Main/Scripts/OldScrips/PatrolEnemyFlip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolEnemyFlip : MonoBehaviour
+{
+    //private bool isPatrol;
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (isPatrol)
+    //    {
+    //        EnemyPatrolController enemy = collision.GetComponent<EnemyPatrolController>();
+    //        if (enemy != null)
+    //        {
+    //            enemy.BackFlip();
+    //        }
+    //    } else
+    //    {
+    //        EnemyFyController enemy = collision.GetComponent<EnemyFyController>();
+    //        if (enemy != null)
+    //        {
+    //            enemy.BackFlip();
+    //        }
+    //    }
+        
+
+    //}
+
+    //public void SetIsPatrol(bool response)
+    //{
+    //    isPatrol = response;
+    //}
+}
